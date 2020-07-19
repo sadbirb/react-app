76c5943ca8ad476d2954ee4e14ef00a29b208fe4
sadbirb <nero1323@gmail.com>
2020-07-19 23:38:47 +0300

    deploy

---
+++ build/precache-manifest.70e4c4297b97549e4a014a13d64a84ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5672a2a75921fde84aea57a95a76ee43",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "3367a61565de07d1d106",
+    "url": "/react-app/static/css/2.840dccc3.chunk.css"
+  },
+  {
+    "revision": "054194c6c200e600ee48",
+    "url": "/react-app/static/css/main.6e6fcd53.chunk.css"
+  },
+  {
+    "revision": "3367a61565de07d1d106",
+    "url": "/react-app/static/js/2.6d72ecb1.chunk.js"
+  },
+  {
+    "revision": "6a54bbc6f8b44cdd37b9f23d3a81f6b8",
+    "url": "/react-app/static/js/2.6d72ecb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "054194c6c200e600ee48",
+    "url": "/react-app/static/js/main.1e1a9c38.chunk.js"
+  },
+  {
+    "revision": "605cf62042abf25f1d08",
+    "url": "/react-app/static/js/runtime-main.056c36fa.js"
+  }
+]);
